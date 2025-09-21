7b0c2dc4b8be2c989686b34ba25f5db1940d07ea
Gajal Gupta <159435865+Gajal636@users.noreply.github.com>
2025-09-21 14:55:24 +0530

    Update Notes.jsx

--- Notes.jsx
+++ Notes.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import api from "../api/axios";
+import api from "/axios";
 
 const Notes = () => {
   const [notes, setNotes] = useState([]);
@@ -104,3 +104,4 @@ const Notes = () => {
 };
 
 export default Notes;
+
