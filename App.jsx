00eec0cf03691e9cb2c5dc292ca9ab24cd45efa1
Gajal Gupta <159435865+Gajal636@users.noreply.github.com>
2025-09-21 15:49:23 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,8 +1,8 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "/Login";
 import Register from "/Register";
 import Notes from "/Notes";
-import React from "react";
 
 function App() {
   return (
