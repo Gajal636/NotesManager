fb76786edf4badab0bb9f544f7456af05722295d
Gajal Gupta <159435865+Gajal636@users.noreply.github.com>
2025-09-21 15:50:18 +0530

    Update axios.js

--- axios.js
+++ axios.js
@@ -1,8 +1,24 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", // backend server
-  withCredentials: true             // allow cookies (for JWT auth)
+  baseURL: 'http://localhost:3000/api', 
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
+
+// Token authentication interceptor
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
