6c683189a5bae14c75d239eb2d68e2224eba06e3
Gajal Gupta <159435865+Gajal636@users.noreply.github.com>
2025-09-21 13:57:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,12 @@ export default defineConfig({
   ],
   server: {
     host: '0.0.0.0',
-    port: process.env.PORT || 5173
+    port: process.env.PORT || 5173,
+    allowedHosts: ['.onrender.com', 'localhost', '127.0.0.1']
   },
   preview: {
     host: '0.0.0.0',
-    port: process.env.PORT || 4173
+    port: process.env.PORT || 4173,
+    allowedHosts: ['.onrender.com', 'localhost', '127.0.0.1']
   }
 })
